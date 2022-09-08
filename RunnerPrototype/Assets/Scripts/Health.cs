51f5a96a88b259f83498c82ddf64840ed0079da6
Никита <99075583+kachanus@users.noreply.github.com>
2022-09-08 21:14:35 +0300

    добавил скрипты

---
+++ RunnerPrototype/Assets/Scripts/Health.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float Hp = 3;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Death")
+        {
+            Hp -= 1;
+            Debug.Log("-1HP");
+
+            if(Hp < 0)
+            {
+                Debug.Log("YOU LOST");
+                Death();
+
+            }
+
+        }
+        
+    }
+    private void Update()
+    {
+        if(transform.position.y <= -2)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        Destroy(gameObject);
+        UnityEditor.EditorApplication.isPaused = true;
+    }
+}
---
+++ RunnerPrototype/Assets/Scripts/Player.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+
+
+public class Player : MonoBehaviour
+{
+    
+    public PlayerControls Controls;
+    public InputAction Test;         
+    public float time;    
+   
+    
+    
+
+
+    
+    public static bool IsGrounded { get; internal set; }
+    public float speed = 10f;
+    private float Speedturns = 15f;
+    
+    internal static object position;
+
+    private void Awake()
+    {
+        Controls = new PlayerControls();
+    }
+   
+
+    private void OnEnable()
+    {
+       Controls.ActionMap.Enable();      
+              
+       
+    }   
+
+    
+
+
+    void Update()
+    {
+
+        transform.Translate (Vector3.forward * speed * Time.deltaTime);   
+        
+
+
+        {
+            time += Time.deltaTime;
+                                   
+                var value = Controls.ActionMap.Movement.ReadValue<Vector2>();
+
+            transform.position += new Vector3(value.x, 0, value.y)* Speedturns * Time.deltaTime;
+        }
+        {           
+
+            if (Input.GetKey(KeyCode.Space))
+             transform.Translate(new Vector3(0, 0.1f, 0));             
+
+
+        }
+
+          
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+
+}
